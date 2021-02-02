cecb9da9ea81f76558f86e6f9d43c029bbf9b1e0
Bonnie Peng <bonniepeng2002@gmail.com>
2021-02-02 11:49:28 -0400

    added W2021 execs

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -5,6 +5,196 @@ import logo from "../images/IMG_7464.PNG";
 
 class OurTeam extends React.Component {
     render() {
+
+        const execs = [
+            {
+                'name': 'Marina Zheng',
+                'role': 'President',
+                'logo': logo
+            },
+            {
+                'name': 'Janice Ng',
+                'role': 'VP Finance',
+                'logo': logo
+            },
+            {
+                'name': 'Tiantian Li',
+                'role': 'VP Admin',
+                'logo': logo
+            },
+            {
+                'name': 'Katie Szajbely',
+                'role': 'Head of Administration',
+                'logo': logo
+            },
+            {
+                'name': 'Nadine Ghanem',
+                'role': 'Head of Events',
+                'logo': logo
+            },
+            {
+                'name': 'Sophia Wong',
+                'role': 'Head of Marketing',
+                'logo': logo
+            },
+            {
+                'name': 'Tanveer Randhawa',
+                'role': 'Head of Sponsorship',
+                'logo': logo
+            },
+            {
+                'name': 'Bonnie Peng',
+                'role': 'Web Design Director',
+                'logo': logo
+            },
+            {
+                'name': 'Aida Zaheer',
+                'role': 'Events Director',
+                'logo': logo
+            },
+            {
+                'name': 'Anique Singh',
+                'role': 'Events Director',
+                'logo': logo
+            },
+            {
+                'name': 'Anna Kandias',
+                'role': 'Events Director',
+                'logo': logo
+            },
+            {
+                'name': 'Hailey Granger',
+                'role': 'Events Director',
+                'logo': logo
+            },
+            {
+                'name': 'Shereen Akkila',
+                'role': 'Events Director',
+                'logo': logo
+            },
+            {
+                'name': 'Amy Chung',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Elina Ho',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Krishna Arunkumar',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Mannat Kapoor',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Meagan Taylor',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Navreet Bhandal',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Parmpreet Mangat',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Sara Sennaoui',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Sarah Ismail',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Savera Lodhy',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Sukhman Garcha',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Victoria Lee',
+                'role': 'Sponsorship Director',
+                'logo': logo
+            },
+            {
+                'name': 'Breanna Quan',
+                'role': 'Marketing Director',
+                'logo': logo
+            },
+            {
+                'name': 'Hetvi Mahida',
+                'role': 'Marketing Director',
+                'logo': logo
+            },
+            {
+                'name': 'Hishany Kulenthiran',
+                'role': 'Marketing Director',
+                'logo': logo
+            },
+            {
+                'name': 'Eesha Mariam',
+                'role': 'Marketing Director',
+                'logo': logo
+            },
+            {
+                'name': 'Nina Doad',
+                'role': 'Marketing Director',
+                'logo': logo
+            },
+            {
+                'name': 'Sarch Darch',
+                'role': 'Marketing Director',
+                'logo': logo
+            },
+            {
+                'name': 'Maira Khan',
+                'role': 'Finance Director',
+                'logo': logo
+            },
+            {
+                'name': 'Victoria Thai',
+                'role': 'Finance Director',
+                'logo': logo
+            },
+            {
+                'name': 'Michelle Wong',
+                'role': 'Administrative  Director',
+                'logo': logo
+            },
+            {
+                'name': 'Tonia Hart',
+                'role': 'Administrative  Director',
+                'logo': logo
+            }
+
+        ];
+
+        const html_code = [];
+        console.log(execs.length);
+        for (let i = 0; i < execs.length; i++) {
+            html_code.push(<div className="exec">
+                <img src={execs[i]['logo']} alt={execs[i]['name']}></img>
+                <h2>{execs[i]['name']}</h2>
+                <p>{execs[i]['role']}</p>
+            </div>)
+        }
+
         return (
             <div className="team">
                 <div className="title">
@@ -12,149 +202,7 @@ class OurTeam extends React.Component {
                 </div>
                 <div className="all-execs">
 
-                    <div className="exec">
-                        <img src={logo} alt="Marina Zheng"></img>
-                        <h2>Marina Zheng</h2>
-                        <p>President</p>
-                    </div>
-                    
-                    <div className="exec">
-                        <img src={logo} alt="Tiantian Li"></img>
-                        <h2>Tiantian Li</h2>
-                        <p>Secretary / VP Admin</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Janice Ng"></img>
-                        <h2>Janice Ng</h2>
-                        <p>VP Finance</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Bonnie Peng"></img>
-                        <h2>Bonnie Peng</h2>
-                        <p>Web Developer</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-                    
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
-
-                    <div className="exec">
-                        <img src={logo} alt="Exec Profile"></img>
-                        <h2>Name</h2>
-                        <p>Role</p>
-                    </div>
+                { html_code }
 
                 </div>
             </div>
