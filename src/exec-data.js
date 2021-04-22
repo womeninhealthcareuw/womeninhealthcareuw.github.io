8127f7ee87b4205c9a4b1aca753b525cfd045332
Bonnie <bonniepeng2002@gmail.com>
2021-04-22 18:20:02 -0300

    adding imag

--- src/exec-data.js
+++ src/exec-data.js
@@ -1,214 +1,198 @@
 export const data = [
   {
-    id: 1,
     name: "Marina Zheng",
     role: "President",
-    image: require(`./images/exec-images/7E7C261A-4AF3-437F-8189-B92171CE30B3_Marina Zheng.JPG`),
+    image:
+      require("./images/exec-images/7E7C261A-4AF3-437F-8189-B92171CE30B3_Marina Zheng.JPG"),
   },
   {
-    id: 2,
     name: "Janice Ng",
     role: "VP Finance",
-    image: require(`./images/IMG_7464.PNG`),
+    image: require("./images/IMG_7464.PNG"),
   },
   {
-    id: 3,
     name: "Tiantian Li",
     role: "VP Admin",
-    image: require(`./images/IMG_7464.PNG`),
+    image: require("./images/IMG_7464.PNG"),
   },
   {
-    id: 4,
     name: "Katie Szajbely",
     role: "Head of Administration",
-    image: require(`./images/exec-images/09B621EB-6F3A-4782-8670-B2FA37A8AAF5_Kathleen Szajbely.png`),
+    image:
+      require("./images/exec-images/09B621EB-6F3A-4782-8670-B2FA37A8AAF5_Kathleen Szajbely.png"),
   },
   {
-    id: 5,
     name: "Nadine Ghanem",
     role: "Head of Events",
-    image: require(`./images/exec-images/119887300_324236568907334_7068601870854881642_Nadine Ghanem.jpg`),
+    image:
+      require("./images/exec-images/119887300_324236568907334_7068601870854881642_Nadine Ghanem.jpg"),
   },
   {
-    id: 6,
     name: "Sophia Wong",
     role: "Head of Marketing",
-    image: require(`./images/exec-images/Sophia Wong - Head of Marketing.jpg`),
+    image: require("./images/exec-images/Sophia Wong - Head of Marketing.jpg"),
   },
   {
-    id: 7,
     name: "Tanveer Randhawa",
     role: "Head of Sponsorship",
-    image: require(`./images/exec-images/Tanveer Randhawa - Head of Sponsorship .jpg`),
+    image: require("./images/exec-images/Tanveer Randhawa - Head of Sponsorship .jpg"),
   },
   {
-    id: 8,
     name: "Bonnie Peng",
     role: "Web Design Director",
-    image: require(`./images/exec-images/CC1D041D-D00A-471E-9608-43CA3695AC43_Bonnie Peng.jpeg`),
+    image:
+      require("./images/exec-images/CC1D041D-D00A-471E-9608-43CA3695AC43_Bonnie Peng.jpeg"),
   },
   {
-    id: 9,
     name: "Aida Zaheer",
     role: "Events Director",
-    image: require(`./images/exec-images/0D1F201D-E352-455A-97CD-FBA2C9E89C53_Aida Zaheer.jpeg`),
+    image:
+      require("./images/exec-images/0D1F201D-E352-455A-97CD-FBA2C9E89C53_Aida Zaheer.jpeg"),
   },
   {
-    id: 10,
     name: "Anique Singh",
     role: "Events Director",
-    image: require(`./images/exec-images/6E2321E4-3517-42E6-8B69-31DE068E2BE0.jpeg`),
+    image: require("./images/exec-images/6E2321E4-3517-42E6-8B69-31DE068E2BE0.jpeg"),
   },
   {
-    id: 11,
     name: "Anna Kandias",
     role: "Events Director",
-    image: require(`./images/IMG_7464.PNG`),
+    image: require("./images/IMG_7464.PNG"),
   },
   {
-    id: 12,
     name: "Hailey Granger",
     role: "Events Director",
-    image: require(`./images/exec-images/IMG_6653_Hailey Elizabeth Gra.PNG`),
+    image: require("./images/exec-images/IMG_6653_Hailey Elizabeth Gra.PNG"),
   },
   {
-    id: 13,
     name: "Shereen Akkila",
     role: "Events Director",
-    image: require(`./images/exec-images/11FEE39C-DFF4-48CA-895E-463AE4F104C2_Shereen Akkila.jpeg`),
+    image:
+      require("./images/exec-images/11FEE39C-DFF4-48CA-895E-463AE4F104C2_Shereen Akkila.jpeg"),
   },
   {
-    id: 14,
     name: "Amy Chung",
     role: "Sponsorship Director",
-    image: require(`./images/exec-images/F974D6E6-15B0-44C3-A57C-05D554D34B7F_Amy Chung.jpeg`),
+    image:
+      require("./images/exec-images/F974D6E6-15B0-44C3-A57C-05D554D34B7F_Amy Chung.jpeg"),
   },
   {
-    id: 15,
     name: "Elina Ho",
     role: "Sponsorship Director",
-    image: require(`./images/exec-images/elina (2)_Elina Ho.jpg`),
+    image: require("./images/exec-images/elina (2)_Elina Ho.jpg"),
   },
   {
-    id: 16,
     name: "Krishna Arunkumar",
     role: "Sponsorship Director",
-    image: require(`./images/exec-images/16E247E0-BE74-4340-B282-39DB0E30FE7B_Krishna Arunkumar.JPG`),
+    image:
+      require("./images/exec-images/16E247E0-BE74-4340-B282-39DB0E30FE7B_Krishna Arunkumar.JPG"),
   },
   {
-    id: 17,
     name: "Mannat Kapoor",
     role: "Sponsorship Director",
-    image: require(`./images/exec-images/D5177698-FB47-49B7-976B-6184B583E676_Mannat Kapoor.jpeg`),
+    image:
+      require("./images/exec-images/D5177698-FB47-49B7-976B-6184B583E676_Mannat Kapoor.jpeg"),
   },
   {
-    id: 18,
     name: "Meagan Taylor",
     role: "Sponsorship Director",
-    image: require(`./images/exec-images/AFDF4E6F-AAB8-4573-BB62-0BD9371E43D4_Meagan Taylor.jpeg`),
+    image:
+      require("./images/exec-images/AFDF4E6F-AAB8-4573-BB62-0BD9371E43D4_Meagan Taylor.jpeg"),
   },
   {
-    id: 19,
     name: "Navreet Bhandal",
     role: "Sponsorship Director",
-    image: require(`./images/exec-images/FB560B84-9C0C-4DE3-81E3-687D8CAE88C4_Navreet Kaur Bhandal.jpeg`),
+    image:
+      require("./images/exec-images/FB560B84-9C0C-4DE3-81E3-687D8CAE88C4_Navreet Kaur Bhandal.jpeg"),
   },
   {
-    id: 20,
     name: "Parmpreet Mangat",
     role: "Sponsorship Director",
-    image: require(`./images/exec-images/8D2A5639-7C8C-491E-BB34-10EA537EF169_Parmpreet Kaur Manga.jpeg`),
+    image:
+      require("./images/exec-images/8D2A5639-7C8C-491E-BB34-10EA537EF169_Parmpreet Kaur Manga.jpeg"),
   },
   {
-    id: 21,
     name: "Sara Sennaoui",
     role: "Sponsorship Director",
-    image: require(`./images/IMG_7464.PNG`),
+    image: require("./images/IMG_7464.PNG"),
   },
   {
-    id: 22,
     name: "Sarah Ismail",
     role: "Sponsorship Director",
-    image: require(`./images/exec-images/IMG_2021_02_06-17_36_08_4290_8F8629AA_Sarah Ismail.jpeg`),
+    image:
+      require("./images/exec-images/IMG_2021_02_06-17_36_08_4290_8F8629AA_Sarah Ismail.jpeg"),
   },
   {
-    id: 23,
     name: "Savera Lodhy",
     role: "Sponsorship Director",
-    image: require(`./images/exec-images/98673B5B-DAA0-4046-82E0-FD7645A3EE6C_Savera Lodhy.jpeg`),
+    image:
+      require("./images/exec-images/98673B5B-DAA0-4046-82E0-FD7645A3EE6C_Savera Lodhy.jpeg"),
   },
   {
-    id: 24,
     name: "Sukhman Garcha",
     role: "Sponsorship Director",
-    image: require(`./images/exec-images/Screenshot_20210201-222343_Sukhman Garcha.jpg`),
+    image:
+      require("./images/exec-images/Screenshot_20210201-222343_Sukhman Garcha.jpg"),
   },
   {
-    id: 25,
     name: "Victoria Lee",
     role: "Sponsorship Director",
-    image: require(`./images/exec-images/IMG_9367 3_Victoria Lee.jpg`),
+    image: require("./images/exec-images/IMG_9367 3_Victoria Lee.jpg"),
   },
   {
-    id: 26,
     name: "Breanna Quan",
     role: "Marketing Director",
-    image: require(`./images/exec-images/IMG_9580 3_Breanna Quan.jpg`),
+    image: require("./images/exec-images/IMG_9580 3_Breanna Quan.jpg"),
   },
   {
-    id: 27,
     name: "Hetvi Mahida",
     role: "Marketing Director",
-    image: require(`./images/exec-images/C1B9D6E1-8272-49F4-A9F9-57F35743FC3D_Hetvi Mahida.jpeg`),
+    image:
+      require("./images/exec-images/C1B9D6E1-8272-49F4-A9F9-57F35743FC3D_Hetvi Mahida.jpeg"),
   },
   {
-    id: 28,
     name: "Hishany Kulenthiran",
     role: "Marketing Director",
-    image: require(`./images/exec-images/27EC68A9-ED10-4DFC-BC1A-3F5CC181D29F_Hishany Kulenthiran.jpeg`),
+    image:
+      require("./images/exec-images/27EC68A9-ED10-4DFC-BC1A-3F5CC181D29F_Hishany Kulenthiran.jpeg"),
   },
   {
-    id: 29,
     name: "Eesha Mariam",
     role: "Marketing Director",
-    image: require(`./images/exec-images/B1883BB4-51DE-4F02-B178-309DF62FD373_Eesha Mariam.jpeg`),
+    image:
+      require("./images/exec-images/B1883BB4-51DE-4F02-B178-309DF62FD373_Eesha Mariam.jpeg"),
   },
   {
-    id: 30,
     name: "Nina Doad",
     role: "Marketing Director",
-    image: require(`./images/exec-images/Nina Doad - Sponsorship Director.jpeg`),
+    image: require("./images/exec-images/Nina Doad - Sponsorship Director.jpeg"),
   },
   {
-    id: 31,
     name: "Sarah Darch",
     role: "Marketing Director",
-    image: require(`./images/exec-images/34D8C93D-7E3F-4A62-A8F4-9FD5CD1E8325_Sarah Darch.jpeg`),
+    image:
+      require("./images/exec-images/34D8C93D-7E3F-4A62-A8F4-9FD5CD1E8325_Sarah Darch.jpeg"),
   },
   {
-    id: 32,
     name: "Maira Khan",
     role: "Finance Director",
-    image: require(`./images/exec-images/2021012301063_Maira Niaz Khan.jpg`),
+    image: require("./images/exec-images/2021012301063_Maira Niaz Khan.jpg"),
   },
   {
-    id: 33,
     name: "Victoria Thai",
     role: "Finance Director",
-    image: require(`./images/IMG_7464.PNG`),
+    image: require("./images/IMG_7464.PNG"),
   },
   {
-    id: 34,
     name: "Michelle Wong",
     role: "Administrative  Director",
-    image: require(`./images/exec-images/IMG_20210207_124834_Michelle Wong.jpg`),
+    image: require("./images/exec-images/IMG_20210207_124834_Michelle Wong.jpg"),
   },
   {
-    id: 35,
     name: "Tonia Hart",
     role: "Administrative  Director",
-    image: require(`./images/exec-images/20210207_091253_227_Tonia Hart.jpg`),
+    image: require("./images/exec-images/20210207_091253_227_Tonia Hart.jpg"),
   },
 ];
 
-export default data;
+//export default data;
